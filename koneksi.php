eddd18a8b6093550c4bc08599b72ac4e7eccbe96
SALSABILA-0 <115692369+SALSABILA-0@users.noreply.github.com>
2022-10-22 11:48:03 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,10 @@
+<?php
+//    $koneksi = new PDO("mysql:host=localhost;dbname=kayla_database", "root", "");
+
+//    $koneksi->query("insert into siswa values ( 2, 'saki')");
+
+//    $koneksi->query("delete from siswa where id=2");
+
+//    $koneksi->query("uptade siswa set id='56' where nama='fuwa' ");
+
+//    $koneksi->query("select 'nama' from 'siswa' where id=56");
